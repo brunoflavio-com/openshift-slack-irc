6a260dbc3ca99f0f4d1494f7cefa6ef3c6229562
Bruno Flávio <bmflavio@gmail.com>
2016-10-24 01:46:36 +0100

    Added a listener for the 8080 port so openshift can verify that the application started.

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 #!/bin/env node
-var slackIRC = require('slack-irc');
-var config = require(process.env.OPENSHIFT_DATA_DIR + '/config.json');
-slackIRC(config);
-
 var express = require('express');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 var listener = express.listen(server_port, server_ip_address, function(){
         console.log('Listening on port ' + listener.address().port); //Listening on port 8888
 });
+
+var slackIRC = require('slack-irc');
+var config = require(process.env.OPENSHIFT_DATA_DIR + '/config.json');
+slackIRC(config);
+
